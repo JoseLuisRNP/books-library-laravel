1d3d3c5903427c3b289d42b4d43bd3882e64cbb4
José Luis Recio <reciojoselu@gmail.com>
2020-02-26 19:19:53 +0100

    Basic reservations with test

--- app/Book.php
+++ app/Book.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Author;
+use App\User;
 
 class Book extends Model
 {
@@ -10,5 +12,37 @@ class Book extends Model
 
     public function path() {
         return '/books/'.$this->id;
+    }
+
+    public function setAuthorIdAttribute($author) {
+        $this->attributes['author_id'] = (Author::firstOrCreate([
+            'name' => $author,
+        ]))->id;
+    }
+
+    public function checkout(User $user) {
+        $this->reservations()->create([
+            'user_id' => $user->id,
+            'checked_out_at' => now(),
+        ]);
+    }
+
+    public function checkin($user) {
+        $reservation = $this->reservations()->where('user_id', $user->id)
+        ->whereNotNull('checked_out_at')
+        ->whereNull('checked_in_at')
+        ->first();
+
+        if(is_null($reservation)) {
+            throw new \Exception();
+        }
+
+        $reservation->update([
+            'checked_in_at' => now(),
+        ]);
+    }
+
+    public function reservations(){
+        return $this->hasMany(Reservation::class);
     }
 }
